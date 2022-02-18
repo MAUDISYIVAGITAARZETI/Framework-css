f753f35db1baec48b430e7181edc3d9ee69aadad
MAUDISYIVAGITAARZETI <99390231+MAUDISYIVAGITAARZETI@users.noreply.github.com>
2022-02-18 10:03:34 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // event pada saat link di klik
-$('.page-scroll').on('click', function(){
+$('.page-scroll').on('click', function(e){
 
 	//ambil isi href
-	var href = $(this).attr('href');
-	console.log(href);
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+	//pindahkan scroll
+	$('body').scrollTop(elemenTujuan.offset().top);
+
+	// e.preventDefault();
 
 });
